48f1b0488ecc624c18a1dc3a554cda6ab06072fa
frnkpsycho <frnkpsycho@gmail.com>
2023-07-19 17:32:10 +0800

    2023-07-20: config waybar

--- home/programs/waybar/default.nix
+++ home/programs/waybar/default.nix
@@ -1,74 +1,88 @@
-{ pkgs
+{ user
+, pkgs
 , lib
-, config
 , ...
-}: {
+}:
+# let mkSpan = abbr: content: "<span color='#8aadf4'>${abbr}</span> ${content}";
+# in
+{
   programs = {
     waybar = {
       enable = true;
       style = builtins.readFile ./waybar.css;
-      systemd.enable = true;
+      systemd = {
+        enable = true;
+        # target = "hyprland-session.target";
+      };
       settings = {
         mainBar = {
           layer = "top";
           position = "top";
-          tray = {
-            height = 22;
-          };
-
-          modules-left = [ "sway/workspaces" "sway/mode" ];
-          #
-          # "sway/mode"
+          height = 27;
+          modules-left = [ "sway/mode" "sway/workspaces" "sway/window" ];
+          
           modules-center = [ "clock" ];
-          modules-right = [ "network" "temperature" "cpu" "memory" "pulseaudio" ];
+          modules-right = 
+            let
+              base = [
+                "idle_inhibitor"
+                "network"
+                "cpu"
+                "memory"
+                "battery"
+                "tray"
+              ];
+            in              
+            base;
+          
           "sway/mode" = {
             format = " {}";
           };
           "sway/workspaces" = {
+            all-outputs = true;
             format = "{name}";
-            disable-scroll = true;
-
-            format-icons = {
-              "1" = "";
-              "2" = "";
-              "3" = "";
-              "4" = "";
-              "5" = "";
-              urgent = "";
-              active = "";
-              default = "";
-            };
+            disable-scroll = false;
           };
-          "wlr/workspaces" = {
+          idle_inhibitor = {
             format = "{icon}";
-            on-click = "activate";
             format-icons = {
-              "1" = "";
-              "2" = "";
-              "3" = "";
-              "4" = "";
-              "5" = "";
-              urgent = "";
-              active = "";
-              default = "";
+              activated = "|";
+              deactivated = "-";
             };
           };
+
+          #"wlr/workspaces" = {
+          #  format = "{icon}";
+          #  on-click = "activate";
+          #  on-scroll-up = "${pkgs.hyprland}/bin/hyprctl dispatch workspace e-1";
+          #  on-scroll-down = "${pkgs.hyprland}/bin/hyprctl dispatch workspace e+1";
+          #};
           "sway/window" = {
             max-length = 80;
             tooltip = false;
           };
+          "tray" = {
+            "icon-size" = 20;
+            "spacing" = 5;
+          };
+          disk = {
+            interval = 30;
+            format = "{percentage_free}% free on {path}";
+          };
           clock = {
             format = "{:%H:%M}";
+            timezone = "Asia/Shanghai";
             format-alt = "{:%a %d %b}";
             format-alt-click = "click-right";
-            tooltip = false;
+            # on-click = "${lib.getExe pkgs.swaylock}";
+            tooltip = true;
           };
           battery = {
             format = "{capacity}% {icon}";
             format-alt = "{time} {icon}";
             format-icons = [ "" "" "" "" "" ];
             format-charging = "{capacity}% ";
-            interval = 30;
+            interval = 10;
             states = {
               warning = 25;
               critical = 10;
@@ -79,29 +93,26 @@
             interval = 1;
             format = "{usage}% ";
             max-length = 10;
-
             min-length = 5;
           };
           memory = {
             interval = 1;
             format = "{}% ";
             max-length = 10;
-
             min-length = 5;
+            tooltip = true;
           };
           network = {
             interval = 1;
-            interface = "wan";
-            format = "{bandwidthDownOctets}";
-            max-length = 10;
-
-            min-length = 8;
+            format-wifi = "󰖩 {essid} {bandwidthTotalBytes}";
+            format-ethernet = "󰀂  {ifname} {bandwidthTotalBytes}";
+            format-linked = "󰖪 {essid} (No IP)";
+            format-disconnected = "󰯡 Disconnected";
+            
           };
-          pulseaudio = {
-            min-length = 5;
+          wireplumber = {
             format = "{volume}% {icon}";
-            format-alt = "{volume} {icon}";
-            format-alt-click = "click-right";
+            on-click = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
             format-muted = "x";
             format-icons = {
               phone = [ " " " " " 墳" " " ];
@@ -115,25 +126,10 @@
             hwmon-path = "/sys/class/hwmon/hwmon1/temp1_input";
             format = "{temperatureC}°C ";
             max-length = 10;
-
             min-length = 5;
+            tooltip = false;
           };
 
-          "custom/spotify" = {
-            interval = 1;
-            return-type = "json";
-            exec = "~/.config/waybar/modules/spotify.sh";
-            exec-if = "pgrep spotify";
-            escape = true;
-          };
-          "custom/storage" = {
-            format = "{} ";
-            format-alt = "{percentage}% ";
-            format-alt-click = "click-right";
-            return-type = "json";
-            interval = 60;
-            exec = "~/.config/waybar/storage.sh";
-          };
           backlight = {
             format = "{icon}";
             format-alt = "{percent}% {icon}";
@@ -141,27 +137,6 @@
             format-icons = [ "" "" ];
             on-scroll-down = "light -A 1";
             on-scroll-up = "light -U 1";
-          };
-          "custom/weather" = {
-            format = "{}";
-            format-alt = "{alt}: {}";
-            format-alt-click = "click-right";
-            interval = 1800;
-            return-type = "json";
-            exec = "~/.config/waybar/weather.sh";
-            exec-if = "ping wttr.in -c1";
-          };
-          idle_inhibitor = {
-            format = "{icon}";
-            format-icons = {
-              activated = "";
-              deactivated = "";
-            };
-            tooltip = false;
-          };
-
-          tray = {
-            icon-size = 18;
           };
         };
       };
